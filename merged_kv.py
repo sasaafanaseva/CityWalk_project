778aac26ff4cbfcadea8e4d09755051138439b8b
Osipenkova Julia Valerievna <112972708+guinti@users.noreply.github.com>
2024-05-19 22:43:06 +0300

    ReviewScreen

--- merged_kv.py
+++ merged_kv.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 # Загрузка всех файлов KV
 def load_kv_files():
 #    Builder.load_file('MapScreen.kv')  #его пока нет и мб не будет
+    Builder.load_file('ReviewScreen.kv')
     Builder.load_file('RouteScreen.kv')
     Builder.load_file('RegisterScreen.kv')
     Builder.load_file('MainScreen.kv')
