efecd5cde5ccde237afd1a8728925e6b8dcbcc39
sasaafanaseva <112332319+sasaafanaseva@users.noreply.github.com>
2024-05-31 23:57:15 +0300

    Update merged_kv.py

--- merged_kv.py
+++ merged_kv.py
@@ -2,7 +2,6 @@ from kivy.lang import Builder
 
 # Загрузка всех файлов KV
 def load_kv_files():
-#    Builder.load_file('MapScreen.kv')  #его пока нет и мб не будет
     Builder.load_file('RouteScreen.kv')
     Builder.load_file('RegisterScreen.kv')
     Builder.load_file('MainScreen.kv')
